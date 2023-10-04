f12bc8c3b8894eb3f9ac9738ac41fca96247f35a
Le Bao Trung Ngo <ngolebaotrung@gmail.com>
2023-10-04 17:28:16 +1100

    update homepage.html

--- frontend/assets/js/dashboards-analytics.js
+++ frontend/assets/js/dashboards-analytics.js
@@ -303,14 +303,14 @@
               color: headingColor,
               fontSize: '15px',
               fontWeight: '600',
-              fontFamily: 'Public Sans'
+              fontFamily: 'DM Sans'
             },
             value: {
               offsetY: -25,
               color: headingColor,
               fontSize: '22px',
               fontWeight: '500',
-              fontFamily: 'Public Sans'
+              fontFamily: 'DM Sans'
             }
           }
         }
@@ -458,7 +458,7 @@
               show: true,
               value: {
                 fontSize: '1.5rem',
-                fontFamily: 'Public Sans',
+                fontFamily: 'DM Sans',
                 color: headingColor,
                 offsetY: -15,
                 formatter: function (val) {
@@ -467,7 +467,7 @@
               },
               name: {
                 offsetY: 20,
-                fontFamily: 'Public Sans'
+                fontFamily: 'DM Sans'
               },
               total: {
                 show: true,
