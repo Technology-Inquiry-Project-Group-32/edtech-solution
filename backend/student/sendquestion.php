f3759471a7f02c44007804bcfa7b5a0189432f88
justin0310 <justinluong88@outlook.com>
2023-10-09 20:16:15 +1100

    student php

---
+++ backend/student/sendquestion.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name = "description"   content="TIP3">
+    <meta name = "author"   content="Cao Tuan Luong">
+    <meta name = "keywords" content="php, mysql">
+    <title>Retrieving record to database</title>
+</head>
+<body>
+<h1>Send Question to Teacher</h1>
+<?php
+    require_once("settings.php");    //connection info
+
+    $conn = @mysqli_connect($host,
+        $user,
+        $pwd,
+        $sql_db
+);
+//Check if connection is successful
+    if(!$conn){
+    // Display error message
+    echo "<p>Database failure</p>";
+}
+    else{
+        $question = htmlspecialchars($_POST["inputquestion"]);
+        $subject = htmlspecialchars($_POST["subject"]);
+         
+    $sql_table = "Question";
+    $query = "INSERT INTO $sql_table(Question, SubjectAreaID, SessionID, StudentID) VALUES ('$question','$subject','777', '104362047')";
+
+//Execute the querry
+    $result = mysqli_query($conn, $query);
+
+    if(!$result){
+            echo "<p class=\"wrong\"> Something is wrong with ", $query, "</p>"; 
+    }
+    else{
+        echo "<p class=\"ok\"> Check Question Successful</p>";
+    }
+
+    //close the connection
+    mysqli_close($conn);
+
+}
+?>
+<button><a href="studentPage.html">Back To Student Page </a></button>
+</body>
+</html>
